076d85d2dfef07e2d1f0578e9e2228f8b12a9e23
Murodkobilov <murodkobilov@inbox.ru>
2023-10-22 13:34:23 +0500

    frist commit-2

---
+++ books/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Book
+from .serializers import BookSerializer
+
+from rest_framework import generics
+
+class BookListApiView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #packages
+    'rest_framework',
+    #local apps
+    'books',
 ]
 
 MIDDLEWARE = [
